1e0d0e40e3ce32a1f7d8c6410bd587e65f1981ce
Aakash Raman <49122515+aakashraman@users.noreply.github.com>
2024-07-02 16:25:10 -0400

    Create pdfmetaextractor.py

---
+++ pdfmetaextractor.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python
+
+import pyPdf
+
+def main():
+    # Enter the location of your PDF
+	filename = <LOCATION_OF_THE_PDF>
+	
+	pdfFile = pyPdf.PdfFileReader(file(filename,'rb'))
+	data = pdfFile.getDocumentInfo()
+
+	print "----Metadata of the file----"
+	
+	for metadata in data:
+		print metadata+ ":" +data[metadata]
+
+if __name__ == '__main__':
+	main()
